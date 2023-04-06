406664daad178c1018fdb93b2f73736768aa3642
rusov63 <rusov63@yahoo.com>
2023-04-06 23:45:12 +0400

    add 3 database - create_tables.sql, and connect main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	user_id int primary key,
+	fullname varchar(100) not null
+);
+
+select * from orders
+
+create table customers
+(
+	user_id int primary key,
+	fullname varchar(100) not null,
+	PhoneNumber varchar(64) not null,
+	image varchar(300)
+);
+
+create table orders
+(
+	user_id int primary key,
+	number_order int references customers(user_id) not null,
+	product text,
+	Quantity int not null
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,25 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+with psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='F9-00-4A-77-5E-3C'
+) as conn:
+    with conn.cursor() as cur:
+        #cur.executemany('insert into employees values (%s, %s)',[(1, 'Ivanov S.V'), (2, 'Petrova V.V')])
+        #cur.executemany('insert into customers values (%s, %s, %s, %s)', [(1, 'Sidorova S.P', '892588', 'smile.jpg'),
+                                                                      #(2, 'Petrova V.V', '58888', 'smile.jpg'),
+                                                                      #(3, 'Lerova C.C.', '84156', 'smile.jpg')])
+        cur.executemany('insert into orders values (%s, %s, %s, %s)',[(1, 1, 'колбаса вареная', 2),
+                                                                      (2, 1, 'сыр Российский', 3),
+                                                                      (3, 1, 'шоколад', 1)])
+
+        cur.execute('select * from orders')
+        conn.commit()
+
+        rows=cur.fetchall()
+        for row in rows:
+            print(row)
+
+        conn.close()
