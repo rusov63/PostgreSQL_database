399bf222a4d201cedd79d4e6beeb486b5550d052
rusov63 <rusov63@yahoo.com>
2023-04-07 11:02:12 +0400

    исправление кода, добавлен try

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,25 +1,26 @@
 import psycopg2
 
-with psycopg2.connect(
+conn = psycopg2.connect(
     host='localhost',
     database='north',
     user='postgres',
-    password='F9-00-4A-77-5E-3C'
-) as conn:
-    with conn.cursor() as cur:
-        #cur.executemany('insert into employees values (%s, %s)',[(1, 'Ivanov S.V'), (2, 'Petrova V.V')])
-        #cur.executemany('insert into customers values (%s, %s, %s, %s)', [(1, 'Sidorova S.P', '892588', 'smile.jpg'),
-                                                                      #(2, 'Petrova V.V', '58888', 'smile.jpg'),
-                                                                      #(3, 'Lerova C.C.', '84156', 'smile.jpg')])
-        cur.executemany('insert into orders values (%s, %s, %s, %s)',[(1, 1, 'колбаса вареная', 2),
-                                                                      (2, 1, 'сыр Российский', 3),
-                                                                      (3, 1, 'шоколад', 1)])
+    password='F9-00-4A-77-5E-3C')
 
-        cur.execute('select * from orders')
-        conn.commit()
+try:
+    with conn:
+        with conn.cursor() as cur:
+            #cur.executemany('insert into employees values (%s, %s)',[(1, 'Ivanov S.V'), (2, 'Petrova V.V')])
+            # cur.executemany('insert into customers values (%s, %s, %s, %s)', [(1, 'Sidorova S.P', '892588', 'smile.jpg'),
+            #                                                               (2, 'Petrova V.V', '58888', 'smile.jpg'),
+            #                                                               (3, 'Lerova C.C.', '84156', 'smile.jpg')])
+            # cur.executemany('insert into orders values (%s, %s, %s, %s)',[(1, 1, 'колбаса вареная', 2),
+            #                                                                  (2, 1, 'сыр Российский', 3),
+            #                                                               (3, 1, 'шоколад', 1)])
 
-        rows=cur.fetchall()
-        for row in rows:
-            print(row)
+            cur.execute('select * from customers')
 
-        conn.close()
+            rows=cur.fetchall()
+            for row in rows:
+                print(row)
+finally:
+    conn.close()
